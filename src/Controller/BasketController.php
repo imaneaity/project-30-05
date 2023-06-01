2f0542fb4c84b0500b6a0d8adddaff65ff94eeeb
Imane Aity <imaneaity@gmail.com>
2023-06-01 11:19:20 +0200

    Panier: Ajout des boutons + et -

--- src/Controller/BasketController.php
+++ src/Controller/BasketController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Pizza;
 use App\Entity\Article;
 use App\Repository\BasketRepository;
+use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -43,4 +44,52 @@ class BasketController extends AbstractController
         return $this->redirectToRoute("app_basket_display") ;
 
     }
+
+
+    #[Route('/mon-panier/{id}/plus', name: 'app_basket_plus')]
+    public function plus(Article $article, ArticleRepository $repository): Response
+    {
+        //ajouter +1 à la quantité
+        $qt = $article->getQuantity();
+        $article->setQuantity($qt+1);
+
+        //save dans la bd
+        $repository->save($article, true);
+
+        //redirection vers le panier
+        return $this->redirectToRoute("app_basket_display");
+    }
+
+
+    
+    #[Route('/mon-panier/{id}/moins', name: 'app_basket_minus')]
+    public function minus(Article $article, ArticleRepository $repository, BasketRepository $basketRepo): Response
+    {
+        //mettre la quantité à -1
+        $qt = $article->getQuantity();
+        $article->setQuantity($qt-1);
+
+        //tester si la qt est à 0
+        if($article->getQuantity() <= 0) {
+            //supprimer l'article du panier
+            //1. recup l'utilisateur puis son panier
+            $user= $this->getUser();
+            $basket= $user->getBasket();
+
+            //2.supprimer de l'entité basket l'article
+            $basket->removeArticle($article);
+
+            //màj du panier dans la bd
+            $basketRepo->save($basket, true);
+
+            //redirection vers le panier
+            return $this->redirectToRoute("app_basket_display");
+        }
+        //save dans la bd
+        $repository->save($article, true);
+
+        //redirection vers le panier
+        return $this->redirectToRoute("app_basket_display");
+    }
+
 }
